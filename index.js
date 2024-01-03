1b083512aeeef772cc8e08d8d54ed02bf3e5301e
Nicolas Roos <nico.roos@swisstxt.ch>
2024-01-03 15:36:56 +0100

    WIP POC

--- index.js
+++ index.js
@@ -1,29 +1,104 @@
-const dgram = require('dgram');
+const http = require('http')
+const fs = require('fs')
 const ffmpeg = require('fluent-ffmpeg');
-const server = dgram.createSocket('udp4');
+const webvtt = require('node-webvtt')
+const koa = require('koa')
+const { koaBody } = require('koa-body')
+const app = new koa()
 
-server.on('listening', (msg) => {
-    const address = server.address();
-    console.log(`UDP Server listening on ${address.address}:${address.port}`);
+let startTime = new Date().getTime()
+let ffmpegCommand = undefined
+const startFFmpeg = function() {
+    ffmpegCommand = ffmpeg()
+    .input('udp://239.192.0.86:3791')
+    .outputOptions(['-map 0:v', '-map 0:a'])
+    .on('start', function() {
+        console.log('start')
+        startTime = new Date().getTime()
+    })
+    .on('error', function(error) {
+        console.log('error', error)
+    })
+    .save('test3.mkv')
+}
 
-    const multicastAddress = '239.255.255.250';
-    server.addMembership(multicastAddress);
-    console.log(`UDP Server listening for multicast packets on ${multicastAddress}`);
+
+const cues = []
 
 
-});
+app.use(koaBody())
+app.use(async ctx => {
 
-server.on('message', (message, remote) => {
-    console.log(`Received multicast message from ${remote.address}:${remote.port}`);
-    // Process the message...
-    // Assuming msg contains the video stream data
-    const ffmpegProcess = ffmpeg()
-        .input(message)
-        .inputFormat('mpegts') // Adjust according to your stream format
-        .output('output.mp4')
-        .on('end', () => console.log('Stream saved'))
-        .on('error', (err) => console.error(err))
-        .run();
-});
+    if(ctx.request.url === '/start') {
+        http.get('http://localhost:8001/livetranscription/start', (resp) => {
+            let data = ''
 
-server.bind(1234); // Repl
+            resp.on('data', (chunk) => {
+                data += chunk
+            })
+
+            resp.on('end', () => {
+                console.log('Response:', data)
+            })
+        })
+        startFFmpeg()
+
+    } else if(ctx.request.url === '/end') {
+        http.get('http://localhost:8001/livetranscription/stop', (resp) => {
+            let data = ''
+
+            resp.on('data', (chunk) => {
+                data += chunk
+            })
+
+            resp.on('end', () => {
+                console.log('Response:', data)
+            })
+        })
+        ffmpegCommand.kill()
+        const secondsSinceStart = Math.round((new Date().getTime() - startTime) / 1000)
+        if(cues.length > 0 && cues[cues.length -1].end === false) {
+            console.log('setting end time on last cue', secondsSinceStart)
+            cues[cues.length -1].end = secondsSinceStart
+        }
+        const vttfile = webvtt.compile({
+            cues: cues,
+            valid: true
+        })
+        fs.writeFileSync('subtitle.vtt', vttfile)
+        console.log('written subtitle file')
+        //console.log('vttfile', vttfile)
+    } else if(ctx.request.url.startsWith('/subtitle')) {
+        //console.log('got subtitle')
+        //console.log(ctx.request.body)
+        const secondsSinceStart = Math.round((new Date().getTime() - startTime) / 1000)
+        const sub = webvtt.parse(ctx.request.body, {strict: false})
+        let cue = undefined
+        if(sub.cues.length > 0) {
+            console.log('setting start time on current cue', secondsSinceStart)
+            cue = sub.cues[0]
+            cue.start = secondsSinceStart
+            cue.end = false
+        }
+        if(cues.length > 0 && cues[cues.length -1].end === false) {
+            console.log('setting end time on last cue', secondsSinceStart)
+            cues[cues.length -1].end = secondsSinceStart
+        }
+        if(cue !== undefined) {
+            console.log('pushing cue', cue)
+            cues.push(cue)
+        }
+        //console.log('subtitle', sub)
+
+    }
+    
+    //console.log('req',ctx)
+    ctx.body = 'Hello world'
+})
+
+
+console.log('Hello')
+app.listen(3000)
+console.log('end')
+
+//ffmpeg -y -i udp://239.253.1.2:5000 -c copy -f segment -segment_atclocktime 1 -segment_time 30 -reset_timestamps 1 -strftime 1 out-%Y%m%dT%H%M.mkv
